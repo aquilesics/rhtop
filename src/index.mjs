6cbf1ec4ba8026732c5d29d879b903d0fa853cf6
aquilesics <aquiles.cout@gmail.com>
2023-04-09 17:01:14 -0300

    so far so good

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,29 @@
-import { h, Component, render } from 'https://esm.sh/preact';
+import { h, render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm'; 
+
+const html = htm.bind(h);
+
+function App(props){
+    return html`
+        <div>
+        ${props.cpus.map((cpu) =>{
+            return html`<div>${cpu.toFixed(2)}% usage</div>`
+        })}
+        </div>
+    `;
+}
 
 
-    let i = 0;
 
-    setInterval(async () => {
-        let response = await fetch('/api/cpus');
-
-        if(response.status !== 200){
-            throw new Error(`Http error: ${response.status}`);
-        }
-        let json = await response.json();
-        
-        const app = h('pre',null,JSON.stringify(json,null,2));
-        render(app,document.body );
-        
-    },1000);
+let i = 0;
+setInterval(async () => {
+    let response = await fetch('/api/cpus');
+    if(response.status !== 200){
+        throw new Error(`Http error: ${response.status}`);
+    }
+    let json = await response.json();
+    
+    const app = h('pre',null,JSON.stringify(json,null,2));
+    render(html`<${App} cpus=${json}></${App}>`,document.body);
+    
+},1000);
